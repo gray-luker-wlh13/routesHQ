9e4a1400106927188fbd64a7ca3c4498b2b009c9
grayluker05 <graysonluker@gmail.com>
2019-11-22 10:14:05 -0700

    added main/proxy and built out controller with data

---
+++ server/Controller/climbCtrl.js
@@ -0,0 +1,26 @@
+let data = [
+    {
+        id: 0,
+        img: 'https://cdn-files.apstatic.com/climb/112512810_medium_1494309566_topo.jpg',
+        name: 'Unknown',
+        grade: 'V3'
+    },
+    {
+        id: 1,
+        img: 'https://cdn-files.apstatic.com/climb/109776027_medium_1494362196.jpg',
+        name: 'Kung Fu',
+        grade: 'V6'
+    },
+    {
+        id: 2,
+        img: 'https://cdn-files.apstatic.com/climb/109595717_medium_1494357610_topo.jpg',
+        name: 'Surfboard Sit-Start',
+        grade: 'V4'
+    }
+];
+
+let id = 3;
+
+module.exports = {
+    
+}
--- server/index.js
+++ server/index.js
@@ -5,4 +5,4 @@ const app = express();
 app.use(express.json());
 
 const PORT = 4020;
-app.listen(PORT, () => console.log(gradient('cyan', 'pink')(`Blaze it on port ${PORT}`)));
+app.listen(PORT, () => console.log(gradient.cristal(`Blaze it on port ${PORT}`)));
